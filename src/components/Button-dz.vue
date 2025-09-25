5b13e335ca581eb27b293da19d095365683c9f95
max-user <arsentiev_m@mail.ru>
2025-09-25 15:16:09 +0300

    2-Button

---
+++ src/components/Button-dz.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+  <button class="button">
+    <slot> </slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  border: none;
+  border-radius: 50px;
+  font-size: 24px;
+  padding: 14px;
+  background: var(--color-bg-button-dz);
+  color: var(--color-primary);
+}
+</style>
